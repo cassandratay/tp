480438e092a5a7801e2d1b5ec2dac0d28d734fa1
cassandratay <cassandratayzx@gmail.com>
2026-03-17 15:57:31 +0800

    Convert DeliveryStatus class to enum

--- src/main/java/seedu/address/model/person/DeliveryStatus.java
+++ src/main/java/seedu/address/model/person/DeliveryStatus.java
@@ -1,66 +1,38 @@
 package seedu.address.model.person;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
-
 /**
  * Represents a Person's delivery status in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidDeliveryStatus(String)}
+ * Guarantees: immutable; can be constructed from valid strings via {@link #fromString(String)}
  */
-public class DeliveryStatus {
+public enum DeliveryStatus {
+    PENDING("Pending"),
+    PREPARING("Preparing"),
+    DELIVERED("Delivered");
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Delivery status should be one of the following: 'pending', 'preparing', 'out-for-delivery', 'delivered'";
-
-    /*
-     * Allowed delivery status values.
-     */
-    public static final String VALIDATION_REGEX = "pending|preparing|out-for-delivery|delivered";
+            "Delivery status should be one of the following: 'pending', 'preparing', 'delivered'";
 
     public final String deliveryStatus;
 
     /**
      * Constructs a {@code DeliveryStatus}.
      *
-     * @param status A valid delivery status.
+     * @param deliveryStatus A valid delivery status.
      */
-    public DeliveryStatus(String status) {
-        requireNonNull(status);
-        checkArgument(isValidDeliveryStatus(status), MESSAGE_CONSTRAINTS);
-        deliveryStatus = status;
+    DeliveryStatus(String deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
     }
-
-    /**
-     * Returns true if a given string is a valid delivery status.
-     */
-    public static boolean isValidDeliveryStatus(String test) {
-        return test.matches(VALIDATION_REGEX);
-    }
-
 
     @Override
     public String toString() {
         return deliveryStatus;
     }
 
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
+    public static DeliveryStatus fromString(String deliveryStatus) {
+        try {
+            return DeliveryStatus.valueOf(deliveryStatus.toUpperCase().trim());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(MESSAGE_CONSTRAINTS);
         }
-
-        // instanceof handles nulls
-        if (!(other instanceof DeliveryStatus)) {
-            return false;
-        }
-
-        DeliveryStatus otherStatus = (DeliveryStatus) other;
-        return deliveryStatus.equals(otherStatus.deliveryStatus);
     }
-
-    @Override
-    public int hashCode() {
-        return deliveryStatus.hashCode();
-    }
-
 }
