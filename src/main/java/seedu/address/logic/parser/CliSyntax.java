951900599c404f5c602fa8e9bcf7a42e64e85271
Lennon <lemoncode8888@gmail.com>
2026-03-24 01:13:20 +0800

    feat: Add new prefix for updated box name

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,10 +10,11 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_BOX = new Prefix("b/");
     public static final Prefix PREFIX_REMARKS = new Prefix("o/");
     public static final Prefix PREFIX_DELIVERY_STATUS = new Prefix("s/");
+    public static final Prefix PREFIX_BOX = new Prefix("b/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_EXPIRY_DATE = new Prefix("ex/");
+    public static final Prefix PREFIX_NEW_BOX = new Prefix("nb/");
 
 }
