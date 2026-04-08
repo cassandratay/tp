755a45e339247e25c99087c17c0a29dd77da003d
cassandratay <cassandratayzx@gmail.com>
2026-04-08 22:10:44 +0800

    Edit add command message usage example for relevance

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -51,9 +51,8 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 123456 "
             + PREFIX_BOX + "box-1:2026-12-31 "
-            + PREFIX_REMARKS + "2 fishburgers "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_REMARKS + "Leave at doorstep "
+            + PREFIX_TAG + "VIP ";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
